c577d2640c738fa915dbc9a22b147badd661dba7
lucasadrisilva <lucasadrisilva@hotmail.com>
2022-10-17 19:52:17 -0300

    feat(WebVSL): Correção na criaçaõ de slides vazios

--- app.py
+++ app.py
@@ -2,117 +2,82 @@ from flask import Flask, render_template
 from flask import request
 from pptx import Presentation
 from pptx.util import Inches, Pt
-from pptx.enum.text import PP_ALIGN ,MSO_ANCHOR
-from datetime import datetime
-from time import sleep
+from pptx.enum.text import PP_ALIGN, MSO_ANCHOR
 import shutil
 from waitress import serve
 
 app = Flask(__name__)
 # app = Flask(__name__, template_folder='tempalte', static_folder='tempalte/assets')
 
+
 @app.route("/")
 def index():
-    return render_template("principal.html")
+  return render_template("principal.html")
+
 
 @app.route("/convert")
 def conversor():
-    return render_template("conversor.html")
+  return render_template("conversor.html")
+
 
 @app.route('/autenticar', methods=['GET'])
 def autenticar():
-    vsl = request.args.get('vsl');
-    vsl = vsl.split('\n')
-    x = []
+  vsl = request.args.get('vsl')
+  vsl = vsl.split('\n')
+  x = []
 
-    #Faz o tratamento da string enviada pelo front
-    #Add em um array e se estiver espaço vazio é excluido automaticamente
-    for item in vsl:
-        st = item.replace('\r', '')
+  #Faz o tratamento da string enviada pelo front
+  #Add em um array e se estiver espaço vazio é excluido automaticamente
+  for item in vsl:
+    st = item.replace('\r', '')
+    st = st.strip()
+    x.append(st)
+  
+  #Remove linhas vazias
+  while ("" in x):
+    x.remove("")
+  print(x)
 
-        if st.find('+'):
-            st = st.replace('+', '')
-            st = st.strip()
-            if st != '':
-                x.append(st)     
+  try:
+    #Abrindo PowerPoint
+    apresentacao = Presentation()
+    apresentacao.slide_width = Inches(16)
+    apresentacao.slide_height = Inches(9)
 
-    try:
-        #Abrindo PowerPoint
-        apresentacao = Presentation()
-        apresentacao.slide_width = Inches(16)
-        apresentacao.slide_height = Inches(9)
+    # Cada item no array ira fazer esse bloco
+    for line in x:
 
-        # Cada item no array ira fazer esse bloco
-        for line in x:
+      #Criando um slide
+      slide = apresentacao.slides.add_slide(apresentacao.slide_layouts[6])
 
-            #Criando um slide
-            slide = apresentacao.slides.add_slide(apresentacao.slide_layouts[6])
-            
+      #Criando e centralizando a TextBox
+      x = Inches(3)
+      y = Inches(3.5)
+      largura = Inches(10)
+      altura = Inches(2)
+      caixa_texto = slide.shapes.add_textbox(x, y, largura, altura)
+      caixa_texto = slide.shapes.add_textbox(x, y, largura, altura)
 
-            #Criando e centralizando a TextBox
-            x = Inches(3)
-            y = Inches(3.5)
-            largura = Inches(10)
-            altura = Inches(2)
-            caixa_texto = slide.shapes.add_textbox(x, y, largura, altura)
-            caixa_texto = slide.shapes.add_textbox(x, y, largura, altura)
+      #Formatando o TextBox
+      caixa_texto.text = line
+      caixa_texto.text_frame.paragraphs[0].alignment = PP_ALIGN.CENTER
+      caixa_texto.text_frame.paragraphs[0].font.size = Pt(40)
+      caixa_texto.text_frame.paragraphs[0].font.name = "Arial"
+      caixa_texto.text_frame.vertical_anchor = MSO_ANCHOR.MIDDLE
+      caixa_texto.text_frame.word_wrap = True
 
-            #Formatando o TextBox
-            caixa_texto.text = line
-            caixa_texto.text_frame.paragraphs[0].alignment = PP_ALIGN.CENTER
-            caixa_texto.text_frame.paragraphs[0].font.size = Pt(40)
-            caixa_texto.text_frame.paragraphs[0].font.name = "Arial"
-            caixa_texto.text_frame.vertical_anchor = MSO_ANCHOR.MIDDLE
-            caixa_texto.text_frame.word_wrap = True
+    #Salvando o arquivo pptx
+    apresentacao.save("VSL.pptx")
+    #Alterando o arquivo par a pasta static
+    shutil.move('VSL.pptx', 'static/VSL.pptx')
 
-        #Salvando o arquivo pptx
-        apresentacao.save("VSL.pptx")  
-        #Alterando o arquivo par a pasta static
-        shutil.move('VSL.pptx', 'static/VSL.pptx')
+  except Exception as e:
+    print(e)
 
-    except Exception as e:
-        print(e)
-
-    return render_template("resultado.html")
-
+  return render_template("resultado.html")
 
 
 # if __name__ == "__main__":
 #     app.run(debug=True)
 
 serve(app, host="0.0.0.0", port=8080)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-############### Exemplo de login
-# @app.route('/login', methods=['POST', 'GET'])
-# def login():
-#     error = None
-#     if request.method == 'POST':
-#         if valid_login(request.form['username'],
-#                        request.form['password']):
-#             return log_the_user_in(request.form['username'])
-#         else:
-#             error = 'Invalid username/password'
-#     # the code below is executed if the request method
-#     # was GET or the credentials were invalid
-#     return render_template('login.html', error=error)
